17f323379b43f69b43766bff197cd526f913e326
Simon Knittel <webdevelopment@simonknittel.de>
2015-10-17 21:39:54 +0200

    DRY

--- gulpfile/_scripts.js
+++ gulpfile/_scripts.js
@@ -13,29 +13,29 @@ var build_base = 'build';
 var source_scripts = source_base + '/assets/scripts';
 var build_scripts = build_base + '/assets/js';
 
+function bundle(parameters) {
+    if (!parameters) {
+        var parameters = '';
+    }
 
-export function dev(callback) {
     var files = fs.readdirSync(source_scripts);
 
     for (var i = 0; i < files.length; i++) {
         if (files[i].indexOf('.js', files[i].length - '.js'.length) !== -1) {
             var file = files[i].slice(0, -3);
-            execSync('jspm bundle-sfx ' + source_scripts + '/' + file + '.js ' + build_scripts + '/' + file + '.js');
+            execSync('jspm bundle-sfx ' + source_scripts + '/' + file + '.js ' + build_scripts + '/' + file + '.js ' + parameters);
         }
     }
 
+    return;
+}
+
+export function dev(callback) {
+    bundle();
     return callback();
 }
 
 export function prod(callback) {
-    var files = fs.readdirSync(source_scripts);
-
-    for (var i = 0; i < files.length; i++) {
-        if (files[i].indexOf('.js', files[i].length - '.js'.length) !== -1) {
-            var file = files[i].slice(0, -3);
-            execSync('jspm bundle-sfx ' + source_scripts + '/' + file + '.js ' + build_scripts + '/' + file + '.js --minify --skip-source-maps');
-        }
-    }
-
+    bundle('--minify --skip-source-maps');
     return callback();
 }
