a72933ae7a308f86e2d56089a63fec08503a069d
Simon Knittel <hallo@simonknittel.de>
2016-02-04 13:09:45 +0100

    Removed deprecated code

--- source/assets/scripts/scripts.js
+++ source/assets/scripts/scripts.js
@@ -4,7 +4,3 @@ document.querySelector('html').classList.remove('no-js');
 // Require and initialize FastClick
 import FastClick from '../../../node_modules/fastclick/lib/fastclick';
 FastClick.attach(document.body);
-
-// Require and initialize Application Cache Handler
-// import * as appcacheHandler from './_modules/_appcache-handler';
-// appcacheHandler.init();
