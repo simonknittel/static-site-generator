8abcecf141e2085eeb34b059d67992a14e3f74bc
Simon Knittel <simon.knittel@hmmh.de>
2016-02-12 13:45:43 +0100

    Removed dummy code

--- source/assets/scripts/scripts.js
+++ source/assets/scripts/scripts.js
@@ -1,6 +1,2 @@
 // Remove no-js class
 document.querySelector('html').classList.remove('no-js');
-
-export function foo() {
-    return 'boo';
-}
--- source/assets/scripts/scripts.spec.js
+++
@@ -1,7 +0,0 @@
-import {foo} from './scripts';
-
-describe('foo', () => {
-    it('should say boo', () => {
-        expect(foo()).toEqual('boo');
-    });
-});
